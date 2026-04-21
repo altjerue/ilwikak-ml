a26abdb057d9897987493748bd80260b83c15281
Jesús Rueda-Becerril <jm.ruebe@gmail.com>
2026-04-21 10:19:47 -0700

    Refactor lac_features function to accept HDU input and localize cosmology selection
    - Updated the lac_features function to take a HDU object instead of a lac_sub object.
    - Moved cosmology selection inside the lac_features function for better encapsulation.
    - Replaced all references to lac_sub with lac_hdu to reflect the new input type.
    - Removed unused data loading and DataFrame creation code for clarity and focus on feature extraction.

--- helper.py
+++ helper.py
@@ -9,9 +9,6 @@ import astropy.units as u
 
 warnings.filterwarnings("ignore")
 
-# Selecting cosmology
-cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
-
 
 def safe_float(col):
     return np.ma.filled(col.data, np.nan).astype(float)
@@ -27,21 +24,24 @@ def safe_str_lower(col):
     )
 
 
-def lac_features(lac_sub):
-    z = safe_float(lac_sub["Redshift"])
+def lac_features(lac_hdu):
+    # Selecting cosmology
+    cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+
+    z = safe_float(lac_hdu["Redshift"])
     z = np.where(z <= 0, np.nan, z)
-    nu = safe_float(lac_sub["nu_syn"])
+    nu = safe_float(lac_hdu["nu_syn"])
     nu = np.where(nu <= 0, np.nan, nu)
-    nf = safe_float(lac_sub["nuFnu_syn"])
+    nf = safe_float(lac_hdu["nuFnu_syn"])
     nf = np.where(nf <= 0, np.nan, nf)
-    hp = safe_float(lac_sub["HE_EPeak"])
-    hn = safe_float(lac_sub["HE_nuFnuPeak"])
+    hp = safe_float(lac_hdu["HE_EPeak"])
+    hn = safe_float(lac_hdu["HE_nuFnuPeak"])
     hn = np.where(hn <= 0, np.nan, hn)
     he_erg = (hn * u.MeV / u.cm**2 / u.s).to(u.erg / u.cm**2 / u.s).value
     cd = np.where((~np.isnan(he_erg)) & (~np.isnan(nf)) & (nf > 0), he_erg / nf, np.nan)
-    ef = safe_float(lac_sub["Energy_Flux100"])
-    vi = safe_float(lac_sub["Variability_Index"])
-    fv = safe_float(lac_sub["Frac_Variability"])
+    ef = safe_float(lac_hdu["Energy_Flux100"])
+    vi = safe_float(lac_hdu["Variability_Index"])
+    fv = safe_float(lac_hdu["Frac_Variability"])
     fv = np.where(fv <= 0, np.nan, fv)
 
     with warnings.catch_warnings():
@@ -56,12 +56,12 @@ def lac_features(lac_sub):
     lum = np.where(~np.isnan(z), 4.0 * np.pi * dl**2 * ef, np.nan)
 
     return dict(
-        class_=safe_str(lac_sub["CLASS"]),
-        sed_class=safe_str(lac_sub["SED_class"]),
+        class_=safe_str_lower(lac_hdu["CLASS"]),
+        sed_class=safe_str_lower(lac_hdu["SED_class"]),
         redshift=z,
-        pl_index=safe_float(lac_sub["PL_Index"]),
-        lp_alpha=safe_float(lac_sub["LP_Index"]),
-        lp_beta=safe_float(lac_sub["LP_beta"]),
+        pl_index=safe_float(lac_hdu["PL_Index"]),
+        lp_alpha=safe_float(lac_hdu["LP_Index"]),
+        lp_beta=safe_float(lac_hdu["LP_beta"]),
         log_nu_syn=np.log10(np.where(nu > 0, nu, np.nan)),
         log_nuFnu_syn=np.log10(np.where(nf > 0, nf, np.nan)),
         log_he_epeak=np.log10(np.where(hp > 0, hp, np.nan)),
@@ -70,22 +70,3 @@ def lac_features(lac_sub):
         var_index=vi,
         frac_var=fv,
     )
-
-
-DATA_DIR = Path("../data")
-LAC_H = DATA_DIR / "table-4LAC-DR3-h.fits"
-MOJAVE = DATA_DIR / "VizieR-MOJAVE-XVII.fit"
-
-lac = Table.read(LAC_H)
-lac_feat = lac_features(lac)
-df1 = pd.DataFrame(
-    {
-        "source_name": [str(v).strip() for v in lac["Source_Name"]],
-        "ra": safe_float(lac["RAJ2000"]),
-        "dec": safe_float(lac["DEJ2000"]),
-        "class": lac_feat["class_"],
-        "sed_class": lac_feat["sed_class"],
-        **{k: v for k, v in lac_feat.items() if k not in ["class_", "sed_class"]},
-    }
-)
-df1.head(10)
